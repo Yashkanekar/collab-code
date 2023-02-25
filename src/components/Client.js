0d7bf32bad1981eca6c6bdd539ead6e5fb395060
Yash Kanekar <kanekaryash.11@gmail.com>
2023-02-25 20:24:35 +0530

    editor page sidebar implemented

---
+++ src/components/Client.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Avatar from "react-avatar";
+export const Client = ({ username }) => {
+  return (
+    <div className="client">
+      <Avatar name={username} size={50} round="14px" />
+      <span className="userName">{username}</span>
+    </div>
+  );
+};
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import { Client } from "../components/Client";
+import Editor from "../components/Editor";
 
 const EditorPage = () => {
-  return <div>EditorPage</div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, name: "Yash k" },
+    { socketId: 2, name: "Raj" },
+  ]);
+  return (
+    <div className="mainWrap">
+      <div className="sideBar">
+        <div className="innerSideBar">
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img
+              alt="Collab-code logo"
+              src="/Codinglogo.png"
+              className="logoImage"
+            />
+          </div>
+          <h1 className="editorTitle">Collab-Code</h1>
+          <h3 style={{ color: "#4aed88" }}>Connected</h3>
+
+          <div className="clientsList">
+            {clients.map((client) => {
+              return <Client username={client.name} key={client.socketId} />;
+            })}
+          </div>
+        </div>
+        <button className="btn copyBtn">Copy Room ID</button>
+        <button className="btn editorLeaveBtn">Leave Room</button>
+      </div>
+      <div className="editorWrap">
+        <Editor />
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;
