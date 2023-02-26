c88d77d8e6abc517b8764b81627ca72f39e50235
Yash Kanekar <kanekaryash.11@gmail.com>
2023-02-26 17:56:39 +0530

    editor component implemented

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/ayu-dark.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  useEffect(() => {
+    async function init() {
+      Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
+        mode: { name: "javascript", json: true },
+        theme: "ayu-dark",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    init();
+  }, []);
+  return <textarea id="realtimeEditor"></textarea>;
 };
 
 export default Editor;
